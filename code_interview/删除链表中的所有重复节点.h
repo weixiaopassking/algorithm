5ab04cc25a106e50b2a3d5f34f2c45db511951b7
wykxwyc <wykxwyc@163.com>
2019-07-16 10:34:29 +0800

    add ListNode delete

--- code_interview/删除链表中的所有重复节点.h
+++ code_interview/删除链表中的所有重复节点.h
@@ -1,4 +1,68 @@
-#ifndef 删除链表中的所以重复节点_H
-#define 删除链表中的所以重复节点_H
+#include <solution.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-#endif // 删除链表中的所以重复节点_H
+/*
+给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+
+示例 1:
+输入: 1->2->3->3->4->4->5
+输出: 1->2->5
+
+示例 2:
+输入: 1->1->1->2->3
+输出: 2->3
+
+思想：
+当前节点cur和cur的下一个节点比较
+1.cur->val==cur->next->val
+    1) 如果cur->val==cur->next->next->val,
+    把cur->next删除，把cur的下下个节点链接到cur上，重新循环
+    2) cur->val！=cur->next->next->val
+    删除cur和cur->next,cur=cur->next->next,last和ret更新
+
+2.cur->val！=cur->next->val && ret==nullptr
+找到第一个不重复的节点作为返回的头指针
+
+3.找到非第一个不重复的节点
+不用删除，cur复制为下一个节点
+*/
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==nullptr || head->next==nullptr) return head;
+
+        ListNode* last=nullptr;
+        ListNode* cur=head;
+        ListNode* next=head->next->next;
+        ListNode* ret=nullptr;
+
+        while(cur!=nullptr && cur->next!=nullptr){
+            if(cur->val==cur->next->val){
+                next=cur->next->next;
+                if(next!=nullptr && cur->val==next->val){
+                    delete cur->next;
+                    cur->next=next;
+                    continue;
+                }
+                delete cur->next;
+                delete cur;
+                cur=next;
+                if(last){
+                    last->next=cur;
+                }else{
+                    ret=cur;
+                }
+            }else if(ret==nullptr){
+                ret=cur;
+                last=cur;
+                cur=cur->next;
+            }else{
+                last=cur;
+                cur=cur->next;
+            }
+        }
+        return ret;
+    }
+};
